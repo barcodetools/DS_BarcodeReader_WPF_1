68bd3e91833fdf42a1b81b6de6f88f46c8e97d34
Mike <info@barcodetools.com>
2018-05-13 20:06:44 +0300

    Barcode Reader for WPF
    Barcode Reader for WPF

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AxBarcodeReaderLib.AxBarcodeDecoder barcodeDecoder;
+        System.Windows.Forms.Integration.WindowsFormsHost host;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Create the interop host control.
+            host = new System.Windows.Forms.Integration.WindowsFormsHost();
+
+            //host.
+            host.Width = 320;
+            host.Height = 240;
+
+            // Create the ActiveX control.
+            barcodeDecoder = new AxBarcodeReaderLib.AxBarcodeDecoder();
+
+            // Assign the ActiveX control as the host control's child.
+            host.Child = barcodeDecoder;
+
+            // Add the interop host control to the Grid control's collection of child controls. 
+            this.grid1.Children.Add(host);
+
+            // sets barcode types
+            barcodeDecoder.BarcodeTypes |= 0x40000000;  //add QRCode decoding
+            barcodeDecoder.SetProperty("LinearVerify", true);
+            barcodeDecoder.ShowImage = true;
+
+            //barcodeDecoder.DecodeFile("c:\\1\\1.jpg");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AxBarcodeReaderLib._IReaderEvents_BarcodeInEventHandler barcodeIn = new AxBarcodeReaderLib._IReaderEvents_BarcodeInEventHandler(OnBarcodeIn);
+            barcodeDecoder.BarcodeIn += barcodeIn;
+
+            barcodeDecoder.VideoStart(0, 0, 0);
+        }
+
+        void OnBarcodeIn(object sender, AxBarcodeReaderLib._IReaderEvents_BarcodeInEvent e)
+        {
+            this.label1.Content = e.barcode;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            barcodeDecoder.VideoStop();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            barcodeDecoder.AboutBox();
+        }
+    }
+}
---
+++ _readme.txt
@@ -0,0 +1,52 @@
+1. Add a reference to the generated ActiveX interoperability assembly placed in interop folder (interop/AxInterop.BarcodeReaderLib.dll).
+
+2. Add a reference to the WindowsFormsIntegration assembly, which is named WindowsFormsIntegration.dll. 
+
+3. Add a reference to the Windows Forms assembly, which is named System.Windows.Forms.dll.
+
+4. Open MainWindow.xaml in the WPF Designer.
+
+5. Name the Grid element grid1. 
+  <Grid Name="grid1">
+
+  </Grid>
+
+6. In Design view or XAML view, select the Window element.
+
+7. In the Properties window, click the Events tab.
+
+8. Double-click the Loaded event.
+
+9. Insert the following code to handle the Loaded event.
+This code creates an instance of the WindowsFormsHost control and adds an instance of the AxBarcodeReader control as its child.
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Create the interop host control.
+            host = new System.Windows.Forms.Integration.WindowsFormsHost();
+
+            //host.
+            host.Width = 320;
+            host.Height = 240;
+
+            // Create the ActiveX control.
+            barcodeDecoder = new AxBarcodeReaderLib.AxBarcodeDecoder();
+
+            // Assign the ActiveX control as the host control's child.
+            host.Child = barcodeDecoder;
+
+            // Add the interop host control to the Grid control's collection of child controls. 
+            this.grid1.Children.Add(host);
+
+            // sets barcode types
+            barcodeDecoder.BarcodeTypes |= 0x40000000;  //add QRCode decoding
+            barcodeDecoder.SetProperty("LinearVerify", true);
+            barcodeDecoder.ShowImage = true;
+
+            //barcodeDecoder.DecodeFile("c:\\1\\1.jpg");
+        }
+
+
+
+http://msdn.microsoft.com/en-us/library/ms742735%28v=vs.110%29.aspx
+
